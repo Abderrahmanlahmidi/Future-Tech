cde458f7df048356c2f68367cf3bb6e4abcb58a0
Abderrahmanlahmidi <Abderrahmanlahmidi@github.com>
2024-01-23 18:44:07 +0100

    fix:the button in the responsive

--- app/Global/Explore.tsx
+++ app/Global/Explore.tsx
@@ -5,6 +5,7 @@ import Image from 'next/image';
 import avatar from "../assets/images/Avatar-1.png";
 
 
+
 type Props = {
 
 }
@@ -34,8 +35,8 @@ function Explore({}: Props) {
             <Image src={Arrow} alt='Arrow'/></button>
         </div>
 
-      <div className='flex px-[80px] py-[80px] max-md:py-[20px] max-md:px-[16px] overflow-x-auto custom-scrollbar-hidden  bg-[#141414] overflow-hidden ' >
-         <div className='flex space-x-[20px] ' >
+      <div className='flex  px-[80px] py-[80px] max-md:py-[20px] max-md:px-[16px] overflow-x-auto custom-scrollbar-hidden  bg-[#141414] overflow-hidden ' >
+         <div className='flex space-x-[20px] relative top-[8px]' >
             {buttons.map((item) => (
                <button className={buttonStyle}>{item}</button>
              ))}
@@ -45,29 +46,32 @@ function Explore({}: Props) {
 
       {/* Testimonials */}
       <div className='grid grid-cols-8 max-md:grid-cols-1 max-md:space-y-[30px] max-md:px-[16px] max-md:py-[40px] px-[80px] py-[60px] bg-[#141414]  border-b border-t border-[#404040]' >
-        <div className='col-span-2 items-start flex space-x-[10px] ' >
+        <div className='col-span-2 items-start flex space-x-[10px] justify-between ' >
+          <div className='flex space-x-[10px]'>
             <Image src={avatar} alt="avatar" />
             <div>
               <h3 className='text-[#FFFFFF] text-[18px] font-[600]' >John Techson</h3>
-              <p className='text-[16px] text-[#98989A]' >Quantum Computing</p>
+              <p className='text-[16px] max-md:text-[14px] text-[#98989A]' >Quantum Computing</p>
             </div>
             
-            <div className='w-[150px] h-[50px]'>
-              <button className='w-[150px] h-[50px] max-md:block items-center max-md:w-full space-x-[4px] border border-1 border-[#262626] rounded-[8px]' >
+          </div>
+          
+            <div className='w-[150px] h-[50px] hidden  max-md:block'>
+              <button className='flex h-[50px] items-center justify-center max-md:w-full space-x-[4px] border border-1 border-[#262626] rounded-[8px]' >
                   <p className='text-[14px] text-Grey' >View Blog</p><Image src={Arrow} alt='Arrow'/>
               </button>
             </div>
             
                           
         </div>
-        <div className='col-span-6 flex items-center'> 
+        <div className='col-span-6 flex items-center' > 
 
 
           <div className='w-full space-y-[24px]'>
-             <h4 className='text-[18px] text-[#98989A]'>October 15, 2023</h4>
+             <h4 className='text-[18px] max-md:text-[16px] text-[#98989A]'>October 15, 2023</h4>
              <div>
-               <h3 className='text-[22px] font-[600] text-[#fff]' >The Quantum Leap in Computing</h3>
-               <p className='text-[#98989A]' >Explore the revolution in quantum computing, its applications, and its potential impact on various industries.</p>
+               <h3 className='text-[22px] font-[600] text-[#fff] max-md:text-[18px]' >The Quantum Leap in Computing</h3>
+               <p className='text-[#98989A] max-md:text-[14px] ' >Explore the revolution in quantum computing, its applications, and its potential impact on various industries.</p>
              </div>
              <div>
 
@@ -78,11 +82,11 @@ function Explore({}: Props) {
         
         
         </div> 
-      </div> 
+      </div>  
       </section>
   ) 
 
 }
 
- 
+  
 export default Explore
